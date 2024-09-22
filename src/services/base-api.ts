b87c6ed4e1de78d89f738fc9bed312ce1c5e2812
Hanna A. Slivankova <annslivankova@gmail.com>
2024-09-22 15:18:54 +0300

    fix: add request headers

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -1,9 +1,13 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {API_URL} from "../app-settings.ts";
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: API_URL,
+    prepareHeaders: (headers) => {
+      headers.set('Content-Type', 'application/json');
+      return headers
+    }
   }),
   endpoints: () => ({}),
   reducerPath: 'baseApi',
