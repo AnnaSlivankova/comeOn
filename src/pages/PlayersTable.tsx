65823cac00d39341be8a629b839e49ee9c0c4811
Hanna A. Slivankova <annslivankova@gmail.com>
2024-10-08 15:07:40 +0300

    add: added reducer test-logic

--- src/pages/PlayersTable.tsx
+++ src/pages/PlayersTable.tsx
@@ -3,9 +3,31 @@ import {Player} from "../services/types.ts";
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
 import {formatDate} from "../common/utils/formatDate.ts";
-// import {EmojiEvents} from "@mui/icons-material";
+import {EmojiEvents} from "@mui/icons-material";
 
 const PlayersTable: FC<Props> = ({items}) => {
+  const aggregatedPlayers = items.reduce((acc, player) => {
+    const key = `${player.name} ${player.surname}`;
+
+    if (!acc[key]) {
+      // Если игрока с таким именем и фамилией ещё нет, добавляем его
+      acc[key] = { ...player };
+    } else {
+      // Если игрок с таким именем и фамилией уже есть, складываем рейтинг
+      acc[key].rating += player.rating;
+      acc[key].score += player.score; // Если нужно также сложить score
+    }
+
+    return acc;
+  }, {} as Record<string, Player>);
+
+  // Преобразуем объект обратно в массив
+  const result = Object.values(aggregatedPlayers);
+
+// Вывод результата
+  console.log(result);
+
+
   return (
     <TableContainer component={Paper} sx={{maxWidth: 650}}>
       <Table aria-label="simple table">
@@ -19,7 +41,7 @@ const PlayersTable: FC<Props> = ({items}) => {
         </TableHead>
 
         <TableBody>
-          {items.map((p, i) => {
+          {result.map((p, i) => {
             const {date, time} = formatDate(p.createdAt)
 
             return (
@@ -27,14 +49,15 @@ const PlayersTable: FC<Props> = ({items}) => {
                 key={p.id}
                 sx={{'&:last-child td, &:last-child th': {border: 0}}}
               >
-                {/*<TableCell component="th" scope="row" align="center">*/}
-                {/*  {i === 0 ? <EmojiEvents sx={{color: 'gold'}}/> :*/}
-                {/*    i === 1 ? <EmojiEvents sx={{color: 'silver'}}/> :*/}
-                {/*      i === 2 ? <EmojiEvents sx={{color: '#cd7f32'}}/> :*/}
-                {/*        i + 1*/}
-                {/*  }*/}
-                {/*</TableCell>*/}
-                <TableCell component="th" scope="row" align="center">{i + 1}</TableCell>
+                <TableCell component="th" scope="row" align="center">
+                  {i === 0 ? <EmojiEvents sx={{color: 'gold'}}/> :
+                    i === 1 ? <EmojiEvents sx={{color: 'silver'}}/> :
+                      i === 2 ? <EmojiEvents sx={{color: '#cd7f32'}}/> :
+                        i + 1
+                  }
+                </TableCell>
+
+                {/*<TableCell component="th" scope="row" align="center">{i + 1}</TableCell>*/}
                 <TableCell align="left">{`${p.name} ${p.surname}`}</TableCell>
                 <TableCell align="center">{p.rating || '🤷🏻‍♀️'}</TableCell>
                 <TableCell align="center">
--- src/pages/PlayersView.tsx
+++ src/pages/PlayersView.tsx
@@ -1,19 +1,20 @@
-import {CircularProgress, Fade, Pagination as TablePagination} from "@mui/material";
+// import {CircularProgress, Fade, Pagination as TablePagination} from "@mui/material";
+import {CircularProgress, Fade} from "@mui/material";
 import {useGetTopPlayersListQuery} from "../services/players.service.ts";
 import {Pagination, Player} from "../services/types.ts";
 import PlayersTable from "./PlayersTable.tsx";
 import Box from "@mui/material/Box";
-import {ChangeEvent, useState} from "react";
+// import {ChangeEvent, useState} from "react";
 
 const PlayersView = () => {
-  const [pageNumber, setPageNumber] = useState<number>(1)
-  const {data, isLoading, isError} = useGetTopPlayersListQuery({pageNumber})
+  // const [pageNumber, setPageNumber] = useState<number>(1)
+  const {data, isLoading, isError} = useGetTopPlayersListQuery({pageNumber: 1})
   console.log(isError)
   const test = data as Pagination<Player>
-  const changePageHandler = (event: ChangeEvent<unknown>, value: number) => {
-    console.log('event', event)
-   setPageNumber(value)
- }
+  //  const changePageHandler = (event: ChangeEvent<unknown>, value: number) => {
+  //    console.log('event', event)
+  //   setPageNumber(value)
+  // }
 
   if (isLoading) {
     return (
@@ -34,9 +35,9 @@ const PlayersView = () => {
   return (
     <>
       {data && (
-        <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: '20px', marginTop:'20px'}}>
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: '20px', marginTop: '20px'}}>
           <PlayersTable items={test.items}/>
-          <TablePagination count={test && test.pagesCount || 1} color="primary" onChange={changePageHandler} page={pageNumber}/>
+          {/*<TablePagination count={test && test.pagesCount || 1} color="primary" onChange={changePageHandler} page={pageNumber}/>*/}
         </div>
       )}
     </>
